5e8eb6f01096ef926a32cbbab0f140814d2b850d
Vedant <vedantgkarale@gmail.com>
2026-06-15 11:26:24 +0530

    Style Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 
 const App = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
+
   return (
-    <div>
-      <form className="flex p-10 justify-between">
-        <input className="p-5 " type="text" placeholder="Enter Notes Heading" />
-        <input className="" type="text" placeholder=""/>
+    <div className="h-screen lg:flex bg-black text-white">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex gap-4 lg:w-1/2 p-10 flex-col items-start"
+      >
+        <h1 className="text-4xl mb-2 font-bold">Add Notes</h1>
+
+        <input
+          type="text"
+          placeholder="Enter Notes Heading"
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded "
+        />
+
+        <textarea
+          type="text"
+          className="px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded "
+          placeholder="Write Details here"
+        />
+
+        <button className="bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded">
+          Add Note
+        </button>
       </form>
+      <div className="lg:w-1/2 lg:border-l-2  p-10">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
+          <div>
+            <h3 className="leading-tight text-lg font-bold"></h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
