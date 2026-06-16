06915dd6a363c00eec774cb0bfaac0d301a2a4bb
Vedant <vedantgkarale@gmail.com>
2026-06-16 10:21:07 +0530

    Functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [Title, setTitle] = useState("");
+  const [Details, setDetails] = useState("");
+
+  const [Task, setTask] = useState([]);
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Form Submitted");
+
+    const copyTask = [...Task];
+
+    copyTask.push({ Title, Details });
+
+    setTask(copyTask);
+    console.log(copyTask);
+
+    setTitle("");
+    setDetails("");
+  };
+
+  const deleteNote = (idx) => {
+    const copyTask = [...Task];
+    copyTask.splice(idx,1)
+
+    setTask(copyTask)
   };
 
   return (
@@ -19,13 +40,21 @@ const App = () => {
         <input
           type="text"
           placeholder="Enter Notes Heading"
-          className="px-5 w-full font-medium py-2 border-2 outline-none rounded "
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded"
+          value={Title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
 
         <textarea
           type="text"
           className="px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded "
           placeholder="Write Details here"
+          value={Details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
         />
 
         <button className="bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded">
@@ -35,9 +64,32 @@ const App = () => {
       <div className="lg:w-1/2 lg:border-l-2  p-10">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
         <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
-          <div>
-            <h3 className="leading-tight text-lg font-bold"></h3>
-          </div>
+          {Task.map(function (elem, idx) {
+            return (
+              <div
+                key={idx}
+                className="relative h-52 w-40 rounded-xl bg-cover py-4 px-2 flex flex-col justify-between item-start text-black bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]"
+              >
+                <div>
+                  <h3 className="leading-tight text-lg font-bold">
+                    {elem.Title}
+                  </h3>
+                  <p className="mt-2 leading-tight font-medium text-gray-700">
+                    {elem.Details}
+                  </p>
+                </div>
+                <button
+                  className="active:scale-95 w-full p-1 text-xs hover:bg-red-500 bg-red-400 cursor-pointer text-white rounded font-bold"
+                  onClick={() => {
+                    deleteNote(idx);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+          <div></div>
         </div>
       </div>
     </div>
